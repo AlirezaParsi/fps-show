de9c198d60fe73ba4faa31d31cf23da622fc6157
Aliza <alirezanajmabadi79@gmail.com>
2025-09-21 23:04:19 +0330

    Update JniTools.java

--- app/src/main/java/com/example/fpsmonitor/JniTools.java
+++ app/src/main/java/com/example/fpsmonitor/JniTools.java
@@ -4,6 +4,12 @@ public class JniTools {
     static {
         System.loadLibrary("tools");
     }
+    
     public static native String getFps();
     public static native int getCpuNum();
+    
+    // متدهای جدید برای مانیتورینگ پیشرفته
+    public static native String getGpuUsage();
+    public static native String getCpuTemperature();
+    public static native String getDeviceModel();
 }
