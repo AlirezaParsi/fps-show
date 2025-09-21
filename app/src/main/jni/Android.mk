6ddc8eb1b334f6fb853323f2b8e1069044e17239
Aliza <alirezanajmabadi79@gmail.com>
2025-09-21 23:19:49 +0330

    Update Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-LOCAL_MODULE := tools
-LOCAL_SRC_FILES := interfaces.c functions.c
-LOCAL_LDLIBS+= -L$(SYSROOT)/usr/lib -llog
+
+LOCAL_MODULE    := tools
+LOCAL_SRC_FILES := functions.c interfaces.c
+LOCAL_LDLIBS    := -llog
+
 include $(BUILD_SHARED_LIBRARY)
